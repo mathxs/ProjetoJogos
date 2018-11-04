16b5a03ec83b4fd6aaebcfc999fd7ab3db5671d5
mathxs <38170401+mathxs@users.noreply.github.com>
2018-11-04 11:55:23 -0200

    update a.a.
    Obs. Salvando para garantir dados em caso de falha energetica
    a.a. rotation e collider funcionando

---
+++ JogoScore/Assets/Script/movimentacao.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentacao : MonoBehaviour {
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    private bool isPinned = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!isPinned)
+        {
+            rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+        }
+	}
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if(col.tag == "Rotator")
+        {
+            transform.SetParent(col.transform);
+            isPinned = true;
+        }
+    }
+
+}
