16ef263576349fe4626391f9525fd1f73185d362
mathxs <38170401+mathxs@users.noreply.github.com>
2018-12-02 00:59:09 -0200

    iniciando jogo 2
    parei na criacao dos materiais.

---
+++ JogoScore/Assets/Script/Jogo2/Spaw.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaw : MonoBehaviour {
+	
+	public float spawnObjetos =1f
+
+	public GameObject forma;
+
+	private float nextSpawn;
+	// Update is called once per frame
+	void Update () {
+
+		if(Time.time >= nextSpawn)
+		{
+			Instantiate(forma,Vector3.zero, Quaternion.identity);
+			nextSpawn = Time.time + 1
+		}
+		
+	}
+}
