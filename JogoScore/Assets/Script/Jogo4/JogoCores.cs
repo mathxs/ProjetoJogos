0da132031f99af7b223b95bcb39a547f1402e2ef
Carlos Alberto <carlinhos_cae@hotmail.com>
2018-12-02 19:10:32 -0200

    jogo4

---
+++ JogoScore/Assets/Script/Jogo4/JogoCores.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class JogoCores : MonoBehaviour {
+
+	public Color[] arraycores;
+	public string[] arraypalavras;
+	public int[] numeros;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		//Inicializa vetor de cores
+		arraycores = new Color[6];
+		arraycores[0] = Color.orange;
+		arraycores[1] = Color.blue;
+		arraycores[2] = Color.green;
+		arraycores[3] = Color.yellow;
+		arraycores[4] = Color.red;
+		arraycores[5] = Color.purple;
+
+		//Inicializa Vetor de Palavras
+		arraypalavras = new string[6];
+		arraypalavras[0] = "Laranja";
+		arraypalavras[1] = "Azul";
+		arraypalavras[2] = "Verde";
+		arraypalavras[3] = "Amarelo";
+		arraypalavras[4] = "Vermelho";
+		arraypalavras[5] = "Roxo";
+
+		numeros[] = new int[6];
+		criasequencia();
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void criasequencia(){
+		srand((unsigned) time(&t));
+
+		for( i = 0 ; i < 5 ; i++ ) {
+		 numeros[i] = (int)(rand() % 50));
+	 }
+ }
+
+
+}
